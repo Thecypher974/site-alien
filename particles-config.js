b2688b1997b1318f62cc12411017a7387df2ce5a
Thecypher974 <pillou2903@gmail.com>
2026-06-01 15:30:29 +0200

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,38 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": { "enable": true, "value_area": 800 }
+    },
+    "color": { "value": "#00fff2" },
+    "shape": { "type": "circle" },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": { "enable": true, "speed": 1, "opacity_min": 0.1, "sync": false }
+    },
+    "size": { "value": 3, "random": true },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00fff2",
+      "opacity": 0.2,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": true, "mode": "grab" },
+      "onclick": { "enable": true, "mode": "push" },
+      "resize": true
+    }
+  },
+  "retina_detect": true
+});
